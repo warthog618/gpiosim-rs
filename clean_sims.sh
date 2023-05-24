8b19c4db5e02deae41fffcb9dd12c1f8302acefb
Kent Gibson <warthog618@gmail.com>
2023-05-24 19:00:15 +0800

    improved cleanup

--- clean_sims.sh
+++ clean_sims.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
-# SPDX-FileCopyrightText: 2022 Kent Gibson <warthog618@gmail.com>
+# SPDX-FileCopyrightText: 2023 Kent Gibson <warthog618@gmail.com>
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
@@ -8,8 +8,8 @@
 # preventing it from cleaning up the sims it created, or if you
 # created a sim using basic_sim.sh.
 
-ls -d /sys/kernel/config/gpio-sim/*/*/*/hog 2>/dev/null | xargs -r rmdir
-ls -d /sys/kernel/config/gpio-sim/*/*/line* 2>/dev/null | xargs -r rmdir
-ls -d /sys/kernel/config/gpio-sim/*/bank* 2>/dev/null | xargs -r rmdir
-ls -d /sys/kernel/config/gpio-sim/* 2>/dev/null | xargs -r rmdir
+find /sys/kernel/config/gpio-sim -type d -name hog -print0 2>/dev/null | xargs -0 -r rmdir
+find /sys/kernel/config/gpio-sim -type d -name "line*" -print0  2>/dev/null | xargs -0 -r rmdir
+find /sys/kernel/config/gpio-sim -type d -name "bank*" -print0 2>/dev/null | xargs -0 -r rmdir
+rmdir /sys/kernel/config/gpio-sim/*
 
